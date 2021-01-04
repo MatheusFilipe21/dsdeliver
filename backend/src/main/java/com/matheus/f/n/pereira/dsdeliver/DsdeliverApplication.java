8f99e7d0fe6bb215649f9a252cc19fc1706daa80
MatheusFilipe21 <matheusfilipe1999@gmail.com>
2021-01-04 20:11:41 -0300

    Project created

---
+++ backend/src/main/java/com/matheus/f/n/pereira/dsdeliver/DsdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.f.n.pereira.dsdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdeliverApplication.class, args);
+	}
+
+}
