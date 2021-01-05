dea402c62b9f96eb32cceaa0788bdaa0b90b7c4a
MatheusFilipe21 <matheusfilipe1999@gmail.com>
2021-01-05 16:21:34 -0300

    PUT /orders/{id}/delivered

--- backend/src/main/java/com/matheus/f/n/pereira/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/matheus/f/n/pereira/dsdeliver/services/OrderService.java
@@ -18,28 +18,37 @@ import com.matheus.f.n.pereira.dsdeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
-	
+
 	@Autowired
 	private OrderRepository repository;
-	
+
 	@Autowired
 	private ProductRepository productRepository;
-	
+
 	@Transactional(readOnly = true)
-	public List<OrderDTO> findAll(){
+	public List<OrderDTO> findAll() {
 		List<Order> list = repository.findOrderWithProducts();
 		return list.stream().map(order -> new OrderDTO(order)).collect(Collectors.toList());
 	}
-	
+
 	@Transactional
-	public OrderDTO insert(OrderDTO dto){
-		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
-		for(ProductDTO p : dto.getProducts()) {
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(),
+				OrderStatus.PENDING);
+		for (ProductDTO p : dto.getProducts()) {
 			Product product = productRepository.getOne(p.getId());
 			order.getProducts().add(product);
 		}
 		order = repository.save(order);
-		
+
+		return new OrderDTO(order);
+	}
+
+	@Transactional
+	public OrderDTO setDelivered(Long id) {
+		Order order = repository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		order = repository.save(order);
 		return new OrderDTO(order);
 	}
 }
