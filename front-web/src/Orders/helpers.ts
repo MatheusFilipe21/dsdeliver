d9af26b542487506f0ae9231a97a4bc753b5d854
MatheusFilipe21 <matheusfilipe1999@gmail.com>
2021-01-08 19:07:19 -0300

    Finish front-web

---
+++ front-web/src/Orders/helpers.ts
@@ -0,0 +1,15 @@
+import { type } from "os";
+import { Product } from "./types";
+
+export function checkIsSelected(selectedProducts: Product[], product: Product){
+    return selectedProducts.some(item => item.id === product.id)
+}
+
+export function formatPrice(price: number){
+    const formatter = new Intl.NumberFormat('pt-br', {
+        style: 'currency',
+        currency: 'BRL',
+
+    });
+    return formatter.format(price)
+}
