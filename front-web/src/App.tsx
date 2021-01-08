4339a97020ba6b955aafb46e38e8b5fa12ad499f
MatheusFilipe21 <matheusfilipe1999@gmail.com>
2021-01-08 00:53:44 -0300

    Add NavBar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
+import NavBar from './navbar';
 
 function App() {
   return (
     <div className="App">
-
+      <NavBar />
     </div>
   );
 }
---
+++ front-web/src/navbar/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+import {ReactComponent as Logo} from './logo.svg';
+
+function NavBar(){
+    return(
+        <nav className="main-navbar">
+            <Logo />
+            <a href="" className="logo-text">DS Delivery</a>
+        </nav>
+    )
+}
+
+export default NavBar;
