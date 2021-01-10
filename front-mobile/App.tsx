ca8501840a8d36bd7196d044a7a208b6823f282d
MatheusFilipe21 <matheusfilipe1999@gmail.com>
2021-01-10 12:27:58 -0300

    Add orders list

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,10 +1,8 @@
+import React from 'react';
 import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
 import AppLoading from 'expo-app-loading';
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import Header from './src/Header';
-import Home from './src/Home';
+import Routes from './src/Routes';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -18,9 +16,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Header />
-      <StatusBar style="auto" />
-      <Home />
+      <Routes />
     </View>
   );
 }
--- front-mobile/src/Home/index.tsx
+++ front-mobile/src/Home/index.tsx
@@ -1,21 +1,25 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { Button, Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import Header from '../Header';
 
 export default function Home() {
 
+    const navigation = useNavigation();
     const handleOnPress = () => {
-
+        navigation.navigate('Orders')
     }
 
     return (
         <>
+            <Header />
             <View style={styles.container}>
                 <Image source={require('../assets/deliveryman.png')} />
                 <Text style={styles.title}>Acompanhe os pedidos e {'\n'} entregue no prazo!</Text>
                 <Text style={styles.subTitle}>Receba todos os pedidos do seu {'\n'} restaurante na palma da sua mão</Text>
             </View>
-            <View style={styles.footer}>    
+            <View style={styles.footer}>
                 <RectButton style={styles.button} onPress={handleOnPress}>
                     <Text style={styles.buttonText}>VER PEDIDOS</Text>
                 </RectButton>
